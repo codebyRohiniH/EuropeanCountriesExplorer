1fb7c4fc6e481cffdaad86bfd9b5674df747a98c
Rohini Hiremath <75560129+codebyRohiniH@users.noreply.github.com>
2025-11-14 15:54:14 +0100

    add react-native-paper UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
