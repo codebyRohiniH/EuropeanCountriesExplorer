a10ca10fe26713b26198ab11200923039cf1c03b
Rohini Hiremath <75560129+codebyRohiniH@users.noreply.github.com>
2025-11-14 16:05:08 +0100

    feat(country-detail): implement detail screen showing name, capital, flag, population, and area

--- src/screens/CountryDetailScreen.tsx
+++ src/screens/CountryDetailScreen.tsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, ScrollView } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/AppNavigator";
+import { Text, Card } from "react-native-paper";
 
-const CountryDetailScreen: React.FC = () => {
- return (
-    <View>
-      <Text>Country Detail Screen</Text>
-    </View>
- );
+type CountryDetailScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  "CountryDetail"
+>;
+
+const CountryDetailScreen: React.FC<CountryDetailScreenProps> = ({ route }) => {
+  const {
+    country: { area, capital, flags, name, population },
+  } = route.params || {};
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Card mode="outlined" style={styles.card}>
+        <Card.Cover source={{ uri: flags.png }} style={styles.flag} />
+        <Card.Content style={styles.content}>
+          <Text variant="headlineMedium" style={styles.name}>
+            {name.common}
+          </Text>
+          <Text variant="bodyLarge" style={styles.detail}>
+            Capital: {capital?.[0] || "N/A"}
+          </Text>
+          <Text variant="bodyLarge" style={styles.detail}>
+            Population: {population.toLocaleString()}
+          </Text>
+          <Text variant="bodyLarge" style={styles.detail}>
+            Area: {area.toLocaleString()} km²
+          </Text>
+        </Card.Content>
+      </Card>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    alignItems: "center",
+  },
+  card: {
+    width: "100%",
+    borderRadius: 16,
+    elevation: 2,
+    backgroundColor: "#eeedecff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+
+  flag: {
+    height: 200,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+  },
+  content: {
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+    alignItems: "flex-start",
+  },
+
+  name: {
+    fontWeight: "bold",
+    marginBottom: 12,
+  },
+
+  detail: {
+    marginBottom: 8,
+  },
+});
 
 export default CountryDetailScreen;
