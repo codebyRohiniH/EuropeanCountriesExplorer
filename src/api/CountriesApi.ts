e4b7b16657c23b3f49e7a67b4620cd238adecffc
Rohini Hiremath <75560129+codebyRohiniH@users.noreply.github.com>
2025-11-14 21:55:37 +0100

    use URLSearchParams to build query params for European countries API request

--- src/api/CountriesApi.ts
+++ src/api/CountriesApi.ts
@@ -1,7 +1,11 @@
 import { Country } from "../types/country";
 
 export const fetchEuropeanCountries = async (): Promise<Country[]> => {
-  const response = await fetch("https://restcountries.com/v3.1/region/europe");
+  const params = new URLSearchParams({
+    fields: "name,flags,capital,population,area",
+  });
+  const url = `https://restcountries.com/v3.1/region/europe?${params.toString()}`;
+  const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch European countries");
   }
