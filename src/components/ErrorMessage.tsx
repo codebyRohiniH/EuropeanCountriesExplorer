ad408e61320f415eb1891a6c767fe3cb95815bc1
Rohini Hiremath <75560129+codebyRohiniH@users.noreply.github.com>
2025-11-14 16:06:19 +0100

    implement error message component for errors

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native-paper";
+
+type ErrorMessageProps = {
+  error: string;
+};
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ error }) => {
+  return (
+    <SafeAreaView style={styles.center}>
+      <Text variant="titleLarge" style={styles.errorText}>
+        {error}
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    color: "#ec6b6bff",
+  },
+});
+
+export default ErrorMessage;
