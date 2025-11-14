af8b9f701665f3be22c815176bae6dd9c64b5451
Rohini Hiremath <75560129+codebyRohiniH@users.noreply.github.com>
2025-11-14 16:06:47 +0100

    implement loading component to show loader

---
+++ src/components/Loading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ActivityIndicator } from "react-native-paper";
+
+const Loading: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.center}>
+      <ActivityIndicator animating size="large" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Loading
